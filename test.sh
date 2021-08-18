9a682de2a28628f9d9fcca36917430489f6c38e1
gotti <18141824+gotti@users.noreply.github.com>
2021-08-18 12:13:31 +0900

    add test pass rate

--- test.sh
+++ test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 files="./riscv-tests/*"
+success=0
+all=0
 for file in $files; do
+  all=$(($all+1))
   ./riscv64-unknown-linux-gnu-objcopy --output-format=binary $file ./out
-  ./target/debug/rs-riscv-sc -t out; echo "$?"
+  ./target/debug/rs-riscv-sc -t out &> /dev/null; output=$?
+  if [ $output -eq 0 ]; then
+    echo "$file Success!!"
+    success=$(($success+1))
+  else
+    echo "$file Failed... $output"
+  fi
 done
+
+echo "test passed: $success"
+echo "test failed: $(($all-$success))"
+echo "$success/$all"
+echo "pass rate  : $(bc <<< "scale=1; 100*$success/$all")%"
