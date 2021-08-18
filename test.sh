1a851bca746b477163997b61301dec8a7fc8db07
gotti <18141824+gotti@users.noreply.github.com>
2021-08-18 11:52:49 +0900

    fix test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 files="./riscv-tests/*"
 for file in $files; do
-  ./riscv64-unknown-elf-objcopy --output-format=binary file ./out
+  ./riscv64-unknown-linux-gnu-objcopy --output-format=binary file ./out
   ./target/debug/rs-riscv-sc -t out
 done
